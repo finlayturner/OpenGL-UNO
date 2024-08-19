704c284d7dfe3ce6d72642c98ae5c18acfe2c595
Finn <finlayturner109@gmail.com>
2024-08-19 13:43:34 +0100

    update

--- OpenGL UNO/main.cpp
+++ OpenGL UNO/main.cpp
@@ -1,12 +1,44 @@
 #include "headers/shader.h"
 #include "headers/stb_image.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window, Shader ourShader, float* varMix);
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+// Window
+const unsigned int WIDTH  = 1920;
+const unsigned int HEIGHT = 1080;
+
+// Camera
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f, 0.0f);
+float lastX = 400, lastY = 300;
+float yaw = -90.0f;
+float pitch = 0.0f;
+float fov = 45.0f;
+bool firstMouse = true;
+
+// Timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// Jump
+float firstJump;
+float currentJump;
+bool isJumping = false;
+
+bool togglePolygon = false;
 
 int main() {
 	
@@ -19,7 +51,7 @@ int main() {
 
 	// Create GLFW window
 	// ------------------
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Goat Coder", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Goat Coder", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -28,6 +60,10 @@ int main() {
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetKeyCallback(window, key_callback);
 
 	// GLAD: load all opengl function pointers
 	// ---------------------------------------
@@ -43,22 +79,63 @@ int main() {
 
 	// Set up vertex data & buffers, and configure vertex attributes
 	// -------------------------------------------------------------
-	float triangleOne[] = {
-		// Positions        // Colors
-		-0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  0.0f, 1.0f, // Top left
-		 0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 1.0f, // Top right
-		-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f, // Bottom left
-		 0.5f, -0.5f, 0.0f,  1.0f, 1.0f, 0.0f,  1.0f, 0.0f // Bottom right
+	float verticies[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
-	unsigned int indicies[] = {
-		0, 1, 2,
-		1, 2, 3
+
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  -2.0f,  0.0f),
+		glm::vec3(1.0f,  -2.0f, 0.0f),
+		glm::vec3(2.0f, -2.0f, 0.0f),
+		glm::vec3(3.0f, -2.0f, 0.0f),
+		glm::vec3(4.0f,  -2.0f, 0.0f),
+		glm::vec3(5.0f, -2.0f, 0.0f),
+		glm::vec3(6.0f,  -2.0f, 0.0f),
+		glm::vec3(7.0f,  -2.0f, 0.0f),
+		glm::vec3(8.0f,  -2.0f, 0.0f),
+		glm::vec3(9.0f, -2.0f, 0.0f)
 	};
-	float texCoords[] = {
-		0.0f, 0.0f,   // Lower-left
-		1.0f, 0.0f,   // Lower-right
-		0.5f, 1.0f    // Top-center
-	};
+
 	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
@@ -69,24 +146,20 @@ int main() {
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triangleOne), triangleOne, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
 	// Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	// Color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
 	// Texture attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// Load texture file and create texture object
 	// -------------------------------------------
-	unsigned int texture[2];
-	glGenTextures(2, texture);
+	unsigned int texture[3];
+	glGenTextures(3, texture);
+
 	// Texture 1
 	// ---------
 	glBindTexture(GL_TEXTURE_2D, texture[0]);
@@ -108,6 +181,7 @@ int main() {
 		std::cout << "Failed to load texture" << std::endl;
 	}
 	stbi_image_free(data);
+
 	// Texture 2
 	// ---------
 	glBindTexture(GL_TEXTURE_2D, texture[1]);
@@ -129,43 +203,120 @@ int main() {
 		std::cout << "Failed to load texture" << std::endl;
 	}
 	stbi_image_free(data);
-	// Polygon mode
-	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// Texture 3
+	// ---------
+	glBindTexture(GL_TEXTURE_2D, texture[2]);
+	// Set texture wrapping options of currently bound texture object
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	// Load and generate texture
+	stbi_set_flip_vertically_on_load(false);
+	data = stbi_load("textures/cobble.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
 
 	// Unbind VAO so other VAO calls dont accidentally modify the current VAO
 	glBindVertexArray(0);
 
-	float varMix = 0.5;
+	// Set uniforms of frag shader to texture locations
 	ourShader.use();
-	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+	ourShader.setInt("texture1", 0);
 	ourShader.setInt("texture2", 1);
-	ourShader.setFloat("varMix", varMix);
+	ourShader.setInt("texture3", 2);
+
+
+	// Enable depth testing
+	// --------------------
+	glEnable(GL_DEPTH_TEST);
 
 	// Main render loop
 	// ----------------
 	while (!glfwWindowShouldClose(window))
 	{
+		// Calculate frame time
+		// --------------------
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		// Apply jumping distance
+		if (isJumping == true)
+		{
+			currentJump = glfwGetTime();
+			cameraPos.y = sin((currentJump - firstJump) * 5) * 2;
+			if (currentJump - firstJump > 0.62831853071)
+			{
+				cameraPos.y = 0.0f;
+				isJumping = false;
+			}
+		}
+
+
 		// Handle input
 		// ------------
-		processInput(window, ourShader, &varMix);
+		processInput(window);
 			
 		// Render
 		// ------
-		glClearColor(0.7f, 0.9f, 0.9f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.3f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// Draw triangle
-		// -----------------
+		// Draw container
+		// --------------
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture[0]);
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture[1]);
+		glActiveTexture(GL_TEXTURE2);
+		glBindTexture(GL_TEXTURE_2D, texture[2]);
+
 		// Activate shader
 		ourShader.use();
-		// Draw triangle
-		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
+		// Create transformations
+		glm::vec3 direction;
+		direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+		direction.y = sin(glm::radians(pitch));
+		direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+		cameraFront = glm::normalize(direction);
+
+		glm::mat4 view;
+		glm::mat4 projection = glm::mat4(1.0f);
+		projection = glm::perspective(glm::radians(fov), 800.0f / 600.0f, 0.1f, 100.0f);
+		view       = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+		// Pass tranformations to shader
+		int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
+		int viewLoc       = glGetUniformLocation(ourShader.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		// Render container
+		glBindVertexArray(VAO);
+		for (unsigned int n = 0; n < 20; n++)
+		{
+			for (unsigned int i = 0; i < 20; i++)
+			{
+				glm::mat4 model = glm::mat4(1.0f);
+				model = glm::translate(model, glm::vec3((float)i, -2.0f, (float)n));
+				float angle = 20.0f * i;
+				model = glm::rotate(model, glm::radians(0.0f), glm::vec3(1.0f, 0.3f, 0.5f));
+				int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+				glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+				glDrawArrays(GL_TRIANGLES, 0, 36);
+			}
+		}
 		// Swap buffers, and poll IO events
 		// --------------------------------
 		glfwSwapBuffers(window);
@@ -187,20 +338,84 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 
 // Process key input, and react accordingly
 // ----------------------------------------
-void processInput(GLFWwindow* window, Shader ourShader, float* varMix)
+void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
-	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-		if (*varMix < 1.0)
+
+	glm::vec3 cameraRight = glm::normalize(glm::cross(cameraFront, cameraUp));
+
+	float cameraSpeed = 4.0f * deltaTime;
+	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+		cameraSpeed = 6.5f * deltaTime;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		cameraPos += cameraSpeed * glm::normalize(glm::cross(cameraUp, cameraRight));
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		cameraPos -= cameraSpeed * glm::normalize(glm::cross(cameraUp, cameraRight));
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		cameraPos -= cameraRight * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		cameraPos += cameraRight * cameraSpeed;
+	
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+	{
+		if (!isJumping)
 		{
-			*varMix = *varMix + 0.005;
-			ourShader.setFloat("varMix", *varMix);
+			firstJump = glfwGetTime();
+			isJumping = true;
 		}
-	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-		if (*varMix > 0.0)
+	}
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos;
+	lastX = xpos;
+	lastY = ypos;
+
+	const float sensitivity = 0.1f;
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw   += xoffset;
+	pitch += yoffset;
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	fov -= (float)yoffset;
+	if (fov < 1.0f)
+		fov = 1.0f;
+	if (fov > 90.0f)
+		fov = 90.0f;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_F && action == GLFW_PRESS)
+	{
+		if (!togglePolygon)
 		{
-			*varMix = *varMix - 0.005;
-			ourShader.setFloat("varMix", *varMix);
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			togglePolygon = true;
 		}
+		else
+		{
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			togglePolygon = false;
+		}
+	}
 }
--- OpenGL UNO/shaders/shader.fs
+++ OpenGL UNO/shaders/shader.fs
@@ -1,14 +1,13 @@
 #version 330 core
-in vec3 ourColor;
 in vec2 TexCoord;
 
 out vec4 FragColor;
 
 uniform sampler2D texture1;
 uniform sampler2D texture2;
-uniform float varMix;
+uniform sampler2D texture3;
 
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), varMix);
+    FragColor = texture(texture3, TexCoord);
 }
